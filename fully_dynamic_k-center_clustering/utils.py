3e2b544d4d2830e5e18c1aece3601775c6e5169e
Tae Min Ha <taemin410@gmail.com>
2019-10-20 15:16:46 +0800

    log change

--- fully_dynamic_k-center_clustering/utils.py
+++ fully_dynamic_k-center_clustering/utils.py
@@ -3,15 +3,13 @@ import random
 
 class Log:
 
-    log_file = None
-    long_log = 0
-
     def __init__(self, path, long_log=0):
-        log_file=path
-        long_log=long_log
+        # self.log_file=path
+        self.long_log=long_log
+        self.enable_log(path)
 
     def log_ceil(self, n, base) -> float:
-        return double(math.ceil(log(n) / log(base)))
+        return float(math.ceil(math.log(n) / log(math.base)))
 
     def shuffle_array(self, array, size) -> None:
         if size:
@@ -21,25 +19,25 @@ class Log:
 
     def enable_log(self, path) -> None:
         if path:
-            log_file = open(path, "w")
-            long_log = 0
+            self.log_file = open(path, "w")
+            self.long_log = 0
 
     def enable_long_log(self, path) -> None:
-        log_file = open(path, "w")
-        long_log = 1
+        self.log_file = open(path, "w")
+        self.long_log = 1
 
     def disable_log(self):
-        if log_file:
-            log_file.close()
+        if self.log_file:
+            self.log_file.close()
 
     def get_log_file(self):
-        return log_file
+        return self.log_file
 
     def has_log(self):
-        return log_file != None
+        return self.log_file != None
 
     def has_long_log(self):
-        return 0 != long_log
+        return 0 != self.long_log
 
 log = Log("test.log", 0)
 #from utils import log
