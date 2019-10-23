5f0143d5a953cd2b2639e47f65d60a3c520ccbdd
Tae Min Ha <taemin410@gmail.com>
2019-10-23 23:34:57 +0800

    __main__ file with argparse outline fix whileloop for data_*

--- fully_dynamic_k-center_clustering/data_fully_adv.py
+++ fully_dynamic_k-center_clustering/data_fully_adv.py
@@ -8,21 +8,39 @@ def fully_adv_distance(x, y):
 def fully_adv_import_points(path, window_length):
 
     point_array=[]
+    counter = 0
 
     with open(path) as fp:
         line = fp.readline()
         while line:
-            splitted_array = line.split('\t')
-            timestamp = int(splitted_array[0])
 
-            lati_loni_array = splitted_array[1].split(' ')
-            latitude = float(lati_loni_array[0])
-            longitude = float(lati_loni_array[1])
+            try:
 
-            geo_point = Geo_point(latitude, longitude)
-            # timestamped_point = Timestamped_point(timestamp, timestamp+window_length, geo_point)
+                splitted_array = line.split('\t')
+                timestamp = int(splitted_array[0])
 
-            point_array.append(geo_point)
+                lati_loni_array = splitted_array[1].split(' ')
+                latitude = float(lati_loni_array[0])
+                longitude = float(lati_loni_array[1])
+
+                geo_point = Geo_point(latitude, longitude)
+                # timestamped_point = Timestamped_point(timestamp, timestamp+window_length, geo_point)
+
+                point_array.append(geo_point)
+                counter +=1
+
+                if counter % 100000 == 0:
+                    print(counter , " lines read... ")
+                line = fp.readline()
+
+            except:
+                counter += 1
+                print("Error in line " , counter)
+                print(line)
+
+                line = fp.readline()
+
+
     fp.close()
 
     return point_array
