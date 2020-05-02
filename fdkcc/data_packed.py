393dcea59081e8693a035b1aabc8f6e083331387
Tae Min Ha <taemin410@gmail.com>
2020-05-02 16:32:09 +0900

    erase unused scripts / erase unnecessary comments

--- fdkcc/data_packed.py
+++ fdkcc/data_packed.py
@@ -9,25 +9,6 @@ def packed_distance(a, b) -> float:
     return point.euclidean_distance(a,b)
 
 
-# def packed_read_point(string, p) -> bool:
-#
-#     tmp = string.split("\t")
-#
-#     if not tmp:
-#         return False
-#
-#     #tokenize and check for format of Geo_point p
-#     p = tmp
-#
-#     if not p.longitude:
-#         return False
-#
-#     if not p.latitude:
-#         return False
-#
-#     return True
-#
-
 def packed_import_points(path):
 
     point_array=[]
@@ -46,7 +27,6 @@ def packed_import_points(path):
                 longitude = float(lati_loni_array[1])
 
                 geo_point = Geo_point(latitude, longitude)
-                # timestamped_point = Timestamped_point(timestamp, timestamp+window_length, geo_point)
 
                 point_array.append(geo_point)
                 counter +=1
